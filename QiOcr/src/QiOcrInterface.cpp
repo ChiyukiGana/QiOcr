3faa61c3c91bf95b7201b9d313b4d86992957182
Chiffon <Chiffon@MOE_CHIFFON>
2025-07-21 23:48:17 +0800

    2025-7-21
    添加接口版本和线程数量
    添加接口释放解决析构不调用

---
+++ QiOcr/src/QiOcrInterface.cpp
@@ -0,0 +1,90 @@
+﻿#include <QiOcrInterface.h>
+#include "QiOcr.h"
+
+#define QIOCR_VERSION 1
+
+struct QiOcrInterfaceDef : QiOcrInterface
+{
+	std::vector<std::string> scan_list(const CImage& image, bool skipDet = false)
+	{
+		return ocr->scan_list(image, skipDet);
+	}
+	std::vector<std::string> scan_list(const RECT& rect_screen, bool skipDet = false)
+	{
+		return ocr->scan_list(rect_screen, skipDet);
+	}
+	std::string scan(const CImage& image, bool skipDet = false)
+	{
+		return ocr->scan(image, skipDet);
+	}
+	std::string scan(const RECT& rect_screen, bool skipDet = false)
+	{
+		return ocr->scan(rect_screen, skipDet);
+	}
+	void release()
+	{
+		if (ocr)
+		{
+			delete ocr;
+			ocr = nullptr;
+		}
+	}
+	QiOcrInterfaceDef(size_t threads = 0) : ocr(new QiOcrTool(threads))
+	{
+	}
+	QiOcrInterfaceDef(void* recData, size_t recSize, void* keyData, size_t keySize, void* detData, size_t detSize, size_t threads = 0) : ocr(new QiOcrTool(recData, recSize, keyData, keySize, detData, detSize, threads))
+	{
+	}
+	~QiOcrInterfaceDef()
+	{
+		release();
+	}
+	QiOcrTool* ocr = nullptr;
+};
+
+#ifdef QIOCR_SHARED
+extern "C" __declspec(dllexport)
+#endif
+size_t _stdcall QiOcrInterfaceVersionInterface()
+{
+	return QIOCR_VERSION;
+}
+
+#ifdef QIOCR_SHARED
+extern "C" __declspec(dllexport)
+#endif
+QiOcrInterface* _stdcall QiOcrInterfaceInitInterface(size_t threads)
+{
+	QiOcrInterfaceDef* ocr = new QiOcrInterfaceDef(threads);
+	if (ocr->ocr->isInit()) return (QiOcrInterface*)ocr;
+	ocr->release();
+	delete ocr;
+	return nullptr;
+}
+
+#ifdef QIOCR_SHARED
+extern "C" __declspec(dllexport)
+#endif
+QiOcrInterface* _stdcall QiOcrInterfaceInitInterfaceFromMemory(void* recData, size_t recSize, void* keysData, size_t keysSize, void* detData, size_t detSize, size_t threads)
+{
+	QiOcrInterfaceDef* ocr = new QiOcrInterfaceDef(recData, recSize, keysData, keysSize, detData, detSize, threads);
+	if (ocr->ocr->isInit()) return (QiOcrInterface*)ocr;
+	ocr->release();
+	delete ocr;
+	return nullptr;
+}
+
+#ifndef QIOCR_SHARED
+size_t QiOcrInterfaceVersion()
+{
+	return QiOcrInterfaceVersionInterface();
+}
+QiOcrInterface* QiOcrInterfaceInit(size_t threads)
+{
+	return QiOcrInterfaceInitInterface(threads);
+}
+QiOcrInterface* QiOcrInterfaceInit(void* recData, size_t recSize, void* keysData, size_t keysSize, void* detData, size_t detSize, size_t threads)
+{
+	return QiOcrInterfaceInitInterfaceFromMemory(recData, recSize, keysData, keysSize, detData, detSize, threads);
+}
+#endif
---
+++ QiOcrTest/QiOcrTest.cpp
@@ -0,0 +1,127 @@
+﻿#pragma once
+#include <fstream>
+#include <iostream>
+
+#define QIOCR_SHARED
+
+#include <QiOcrInterface.h>
+
+static bool readFile(const std::string& file, std::unique_ptr<char[]>& data, size_t& size)
+{
+	std::ifstream modelFile(file, std::ios::in | std::ios::binary | std::ios::ate);
+	if (!modelFile) return false;
+
+	size = modelFile.tellg();
+	if (!size) return false;
+
+	modelFile.seekg(0, std::ios::beg);
+	data = std::make_unique<char[]>(size);
+	modelFile.read(data.get(), size);
+	return (bool)modelFile.gcount();
+}
+
+int main()
+{
+	std::locale::global(std::locale(".UTF8"));
+
+	bool speedTest = true;
+	bool loadFromMemory = true;
+	size_t threads = 0;
+
+	size_t ver = QiOcrInterfaceVersion();
+
+	std::cout << "qiocr version: " << ver << "\n" << std::endl;
+
+	QiOcrModule ocr;
+	if (loadFromMemory)
+	{
+		std::unique_ptr<char[]> rec;
+		size_t recSize;
+		if (!readFile("OCR\\ppocr.onnx", rec, recSize)) return -1;
+		std::unique_ptr<char[]> keys;
+		size_t keysSize;
+		if (!readFile("OCR\\ppocr.keys", keys, keysSize)) return -1;
+		std::unique_ptr<char[]> det;
+		size_t detSize;
+		if (!readFile("OCR\\ppdet.onnx", det, detSize)) return -1;
+
+		ocr = QiOcrInterfaceInit(rec.get(), recSize, keys.get(), keysSize, det.get(), detSize, threads);
+	}
+	else
+	{
+		ocr = QiOcrInterfaceInit(threads);
+	}
+	if (!ocr.valid())
+	{
+		std::cout << "OCR failed to init";
+		return -1;
+	}
+
+	if (speedTest)
+	{
+		CImage image;
+		image.Load(L"test.png");
+		if (image.IsNull())
+		{
+			std::cout << "no image";
+			return -1;
+		}
+		while (true)
+		{
+			clock_t begin = clock();
+			std::vector<std::string> result = ocr.scan_list(image);
+			clock_t end = clock() - begin;
+
+			std::cout << "time(ms): " << end << "\n" << std::endl;
+		}
+		return 0;
+	}
+
+	std::cout << "document mode:\n" << std::endl;
+	{
+		CImage image;
+		image.Load(L"test.png");
+		if (image.IsNull())
+		{
+			std::cout << "no image";
+			return -1;
+		}
+
+		clock_t begin = clock();
+		std::vector<std::string> result = ocr.scan_list(image);
+		clock_t end = clock() - begin;
+
+		std::cout << "time(ms): " << end << "\n" << std::endl;
+
+		std::cout << "------------------------------" << std::endl;
+		for (const std::string& i : result)
+		{
+			std::cout << i << std::endl;
+		}
+		std::cout << "------------------------------" << std::endl;
+	}
+	std::cout << "\nline mode:\n" << std::endl;
+	{
+		CImage image;
+		image.Load(L"test2.png");
+		if (image.IsNull())
+		{
+			std::cout << "no image";
+			return -1;
+		}
+
+		clock_t begin = clock();
+		std::string result = ocr.scan(image, true);
+		clock_t end = clock() - begin;
+
+		std::cout << "time(ms): " << end << "\n" << std::endl;
+
+		std::cout << "------------------------------" << std::endl;
+		std::cout << result << std::endl;
+		std::cout << "------------------------------" << std::endl;
+	}
+
+	std::cout << "\n" << std::endl;
+	system("pause");
+	return 0;
+}
